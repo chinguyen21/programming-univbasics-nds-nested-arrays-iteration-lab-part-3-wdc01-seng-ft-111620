0fdaa5ed7f4449c1540e03addf42f3e781452926
Chi Nguyen <nguyen.thimai@northeastern.edu>
2020-10-22 20:01:12 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,11 +1,11 @@
 def join_nested_strings(src)
   row_index = 0 
-  string = ""
+  array
   while row_index < src.length do
     element_index = 0 
     while element_index < src[row_index].length do 
       if src[row_index][element_index].class == String
-        string += src[row_index][element_index]
+        array <<  src[row_index][element_index]
       end
       element_index += 1
     end
